8d755f4ac7eed1de4ed62fda6ee8436b940fedde
matan maron <matanmaron@gmail.com>
2025-07-22 12:09:42 +0900

    add PersistBetweenScenes override

--- Assets/MaronByteStudio/Singleton.cs
+++ Assets/MaronByteStudio/Singleton.cs
@@ -1,54 +1,73 @@
 /**
  * Singleton.cs
  * Author: Luke Holland (http://lukeholland.me/)
+ * Modified by: MaronByteStudio
  */
 using UnityEngine;
 
 namespace MaronByteStudio
 {
-	public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
-	{
-		private static T _instance;
-		private static readonly object _instanceLock = new object();
-		private static bool _quitting = false;
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+        private static readonly object _instanceLock = new object();
+        private static bool _quitting = false;
 
-		public static T instance
-		{
-			get
-			{
-				lock (_instanceLock)
-				{
-					if (_instance == null && !_quitting)
-					{
+        /// <summary>
+        /// Override this property in your derived singleton class to prevent DontDestroyOnLoad.
+        /// ex: protected override bool PersistBetweenScenes => true;
+        /// </summary>
+        protected virtual bool PersistBetweenScenes => false;
 
-						_instance = GameObject.FindObjectOfType<T>();
-						if (_instance == null)
-						{
-							GameObject go = new GameObject(typeof(T).ToString());
-							_instance = go.AddComponent<T>();
+        public static T instance
+        {
+            get
+            {
+                lock (_instanceLock)
+                {
+                    if (_instance == null && !_quitting)
+                    {
+                        _instance = GameObject.FindObjectOfType<T>();
+                        if (_instance == null)
+                        {
+                            GameObject go = new GameObject(typeof(T).ToString());
+                            _instance = go.AddComponent<T>();
 
-							DontDestroyOnLoad(_instance.gameObject);
-						}
-					}
+                            // Handle persistence here
+                            Singleton<T> singletonScript = _instance as Singleton<T>;
+                            if (singletonScript != null && singletonScript.PersistBetweenScenes)
+                            {
+                                DontDestroyOnLoad(_instance.gameObject);
+                            }
+                        }
+                    }
 
-					return _instance;
-				}
-			}
-		}
+                    return _instance;
+                }
+            }
+        }
 
-		protected virtual void Awake()
-		{
-			if (_instance == null) _instance = gameObject.GetComponent<T>();
-			else if (_instance.GetInstanceID() != GetInstanceID())
-			{
-				Destroy(gameObject);
-				throw new System.Exception(string.Format("Instance of {0} already exists, removing {1}", GetType().FullName, ToString()));
-			}
-		}
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = gameObject.GetComponent<T>();
 
-		protected virtual void OnApplicationQuit()
-		{
-			_quitting = true;
-		}
-	}
+                if (PersistBetweenScenes)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+            else if (_instance.GetInstanceID() != GetInstanceID())
+            {
+                Destroy(gameObject);
+                throw new System.Exception($"Instance of {GetType().FullName} already exists, removing {ToString()}");
+            }
+        }
+
+        protected virtual void OnApplicationQuit()
+        {
+            _quitting = true;
+        }
+    }
 }
